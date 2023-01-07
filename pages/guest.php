2f69b33e1ca6838520d5dd4ef0bd5a833090174c
burcu apaydin <burcuuapaydin@gmail.com>
2023-01-08 00:44:22 +0200

    initial commit

---
+++ pages/guest.php
@@ -0,0 +1,12 @@
+<head>
+    <?php include('header.php') ?>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <i class="fa-solid fa-user"></i>
+    <p id="comment"></p>
+    <script>
+        $("#comment").append("comment");
+    </script>
+</body>
